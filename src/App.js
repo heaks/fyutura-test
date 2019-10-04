7485ec544272fc46fed2fa6216e94bbc27ca5e27
captify-vyshnevsky <viktor.vyshnevsky@captify.co.uk>
2019-10-04 18:27:37 +0300

    refactored

--- src/App.js
+++ src/App.js
@@ -5,57 +5,29 @@ import LineChart from './LineChart';
 import BarChart from './BarChart';
 import Controller from './ThresholdControl';
 
+const showSnackbar = text =>
+  SnackBar.show({
+    text,
+    pos: 'bottom-center',
+    backgroundColor: '#323232',
+    textColor: '#ffffff',
+  });
+
+const socket = io('http://localhost:3000');
+
 function App () {
   const [graphData, setGraphData] = useState([]);
   const [threshold, setThreshold] = useState(100);
-  
-  const onData = data => {
-    if (data.value > threshold) {
-      SnackBar.show({
-        text: `Warning, the value is: ${data.value.toFixed(4)}`,
-        pos: 'bottom-center',
-        backgroundColor: '#323232',
-        textColor: '#ffffff',
-      });
-    }
-    setGraphData(value => [...value, data])
-  };
-  
+
   useEffect(() => {
-    const socket = io('http://localhost:3000');
-
-    socket.on('connect', () => {
-      console.log('SOCKET CLIENT CONNECTED');
+    socket.on('data', (data) => {
+      if (data.value > threshold) {
+        showSnackbar(`Warning, the value is: ${data.value.toFixed(4)}`);
+      }
+      setGraphData(value => [...value, data])
     });
+  }, [threshold]);
 
-    socket.on('data', onData);
-    return () => socket.close();
-  }, []);
-  
-  // TODO: alternative
-  // useEffect(() => {
-  //   socket.on('connect', () => {
-  //     console.log('SOCKET CLIENT CONNECTED');
-  //   });
-  //   return () => socket.close();
-  // }, []);
-  //
-  // useEffect(() => {
-  //   socket.on('data', (data) => {
-  //     console.log('threshold:', threshold);
-  //     console.log('data.value:', data.value);
-  //     if (data.value > threshold) {
-  //       SnackBar.show({
-  //         text: `Warning, the value is: ${data.value.toFixed(4)}`,
-  //         pos: 'bottom-center',
-  //         backgroundColor: '#323232',
-  //         textColor: '#ffffff',
-  //       });
-  //     }
-  //     setGraphData(value => [...value, data])
-  //   });
-  // }, [threshold]);
-  
   return (
     <div className="App">
       <div style={{ textAlign: 'center' }}>
